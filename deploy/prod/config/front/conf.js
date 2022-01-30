7b20e4b3f7d70a10df041b76bbf2eaaefadbf305
Jonathan Guichard <jona.guich69@gmail.com>
2022-01-30 01:35:05 +0100

    Deploy -- Feature -- Init project

---
+++ deploy/prod/config/front/conf.js
@@ -0,0 +1,9 @@
+window.config = {
+	endpoints: {
+		core: "https://elyspio.fr/videos",
+		authentication: "https://elyspio.fr/authentication",
+		hubs: "https://elyspio.fr/videos/hubs",
+	},
+	loginPageUrl: "https://elyspio.fr/authentication",
+	videoUrlTemplate: "https://elyspio.fr/files/api/files/user/{id}/stream",
+};
