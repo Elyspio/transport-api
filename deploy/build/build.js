79a2dc82da8ad929cac37662bea22a080a1ca204
Jonathan Guichard <jona.guich69@gmail.com>
2022-01-30 01:36:48 +0100

    Deploy -- Feature -- Add dockerfile

---
+++ deploy/build/build.js
@@ -0,0 +1,8 @@
+const { spawnSync } = require("child_process");
+const path = require("path");
+
+const dockerCommand = `docker buildx build --platform linux/amd64  -f ${__dirname}/Dockerfile  -t elyspio/video-share:latest --push .`.split(" ").filter((str) => str.length);
+
+spawnSync(dockerCommand[0], dockerCommand.slice(1), { cwd: path.resolve(__dirname, "../../"), stdio: "inherit" });
+
+spawnSync("ssh", ["elyspio@192.168.0.59", "cd /apps/own/video-share && docker-compose pull && docker-compose up -d"], { cwd: __dirname, stdio: "inherit" });
