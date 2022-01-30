669fab410ef7eca9ff3f3d4ccf8d16808712c438
Jonathan Guichard <jona.guich69@gmail.com>
2022-01-30 01:35:26 +0100

    Mobile -- Feature -- Add web support

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,12 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useCachedResources } from "./src/view/hooks/useCachedResources";
-import { useColorScheme } from "./src/view/hooks/useColorScheme";
 import Navigation from "./src/view/navigation";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
 import { Provider as PaperProvider } from "react-native-paper";
 import { theme } from "./src/view/constants/Colors";
+import { useColorScheme } from "react-native";
 
 export default function App() {
 	const isLoadingComplete = useCachedResources();
