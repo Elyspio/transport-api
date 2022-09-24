1512c69ef287150a3ce9e15caf0cd31fd3a43584
Jonathan Guichard <jona.guich69@gmail.com>
2022-09-24 16:13:24 +0200

    Mobile -- Fix -- server's url

--- mobile/src/core/apis/index.ts
+++ mobile/src/core/apis/index.ts
@@ -1,11 +1,11 @@
 import { FuelStationsApi } from "./backend/generated";
 import axios from "axios";
 
-const url = process.env.NODE_ENV === "development" ? "http://192.168.0.44:4000" : "https://elyspio.fr/transport";
+const url = "https://elyspio.fr/transport";
 
 const instance = axios.create({
 	baseURL: url,
 	timeout: 1000,
 });
 
-export const fuelStationApi = new FuelStationsApi(undefined, url, axios);
+export const fuelStationApi = new FuelStationsApi(undefined, url, instance);
